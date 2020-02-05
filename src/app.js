fd14dffd78e18fe848f5d9d37f2b0f6e082eab75
João Soares <jpcirqueira81@gmail.com>
2020-02-04 21:01:57 -0300

    ajustando imports.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
-const routes = require('./routes');
+import routes from './routes';
 
-const express = require('express');
+import express from 'express';
 
 class App {
     constructor() {
@@ -18,4 +18,4 @@ class App {
     }    
 }
 
-module.exports = new App().server;
+export default new App().server;
